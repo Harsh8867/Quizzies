62aa186b92c2bb04383fa578738fb605b34bf64c
Harsh <harsh.sharma280196@gmail.com>
2022-08-30 14:52:04 +0530

    Quizzical first screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import { useState } from 'react'
-import quizLogo from './assets/quizzicalLogo.png'
 import './App.css'
+import Quizzies from './quiz'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={quizLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> 
-      <button className="quizBtn">
-        Start Quiz!
-      </button>
-    </div>
+    <Quizzies/>
   )
 }
 
---
+++ src/quiz.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import quizLogo from './assets/quizzicalLogo.png'
+import './App.css'
+import { useEffect } from "react";
+
+export default function Quizzies (){
+    let startQuiz = true;
+     function changeQuizState(){
+        startQuiz = false
+        console.log("working")
+     }
+
+    return (
+        startQuiz  ? 
+        <div className="App">
+        <div>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={quizLogo} className="logo react" alt="React logo" />
+          </a>
+        </div> 
+        <button className="bn632-hover bn26" onClick={changeQuizState}>
+          Start Quiz!
+        </button>
+      </div> 
+      :
+      <div className="App">
+      <button className="bn632-hover bn24">
+        Start Quiz!
+      </button>
+    </div> 
+    )
+}
